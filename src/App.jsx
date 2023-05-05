64bb01e80eb1d7ca17f0574a45f03606fec39bcb
Christie Renaud <mail@christierenaud.com>
2023-05-05 13:43:51 -0400

    Adds NotFound component, loading state, error state and error handling

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import HostLayout from './components/HostLayout'
 import HostVanInfo from './pages/Host/HostVanInfo'
 import HostVanPricing from './pages/Host/HostVanPricing'
 import HostVanPhotos from './pages/Host/HostVanPhotos'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
@@ -37,6 +38,7 @@ function App() {
                 <Route path="photos" element={<HostVanPhotos />} />
               </Route>
             </Route>
+            <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- src/pages/Vans/VanDetail.jsx
+++ src/pages/Vans/VanDetail.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams, useLocation } from 'react-router-dom'
 
 function VanDetail() {
   const params = useParams()
+  const location = useLocation()
   const [van, setVan] = useState(null)
 
   useEffect(() => {
@@ -10,9 +11,14 @@ function VanDetail() {
       .then((res) => res.json())
       .then((data) => setVan(data.vans))
   }, [params.id])
+  const search = location.state?.search || ''
+  const type = location.state?.type || 'all'
 
   return (
     <div className="van-detail-container">
+      <Link to={`..${search}`} relative="path" className="back-button">
+        &larr; <span>Back to {type} vans</span>
+      </Link>
       {van ? (
         <div className="van-detail">
           <img src={van.imageUrl}></img>
